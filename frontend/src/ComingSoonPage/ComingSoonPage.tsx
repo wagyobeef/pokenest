a487927a8090d4e51c30fc4b948ee6545411388d
Grant Kim <grant@Grants-MacBook-Pro.local>
2025-05-01 19:54:06 -0700

    add coming soon page

--- frontend/src/ComingSoonPage/ComingSoonPage.tsx
+++ frontend/src/ComingSoonPage/ComingSoonPage.tsx
@@ -3,12 +3,14 @@ import React from "react";
 const ComingSoonPage: React.FC = () => {
   return (
     <div
-      className="min-h-screen flex items-center justify-center"
+      className="min-h-screen flex flex-col items-center justify-center"
       style={{ backgroundColor: "#FFF9DB" }}
     >
-      <h1 className="text-6xl font-bold" style={{ color: "#F28B82" }}>
-        Coming Soon
+      <h1 className="font-bold mb-4" style={{ fontSize: "5rem" }}>
+        <span className="text-black">Poke</span>
+        <span style={{ color: "#F28B82" }}>Nest</span>
       </h1>
+      <h2 className="text-4xl text-gray-400">Coming Soon</h2>
     </div>
   );
 };
